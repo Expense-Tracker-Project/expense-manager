382b0162c029910f05bbf026375153fc2d814b52
harshitab07 <07harshu07@gmail.com>
2023-07-15 18:58:14 +0530

    new structure added

---
+++ src/components/button/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const Button = ( props ) => {
+return (
+    <button className={`custom-button ${props.className}`} onClick={props.onClick}>{props.title}</button>
+)
+}
+
+export default Button;
