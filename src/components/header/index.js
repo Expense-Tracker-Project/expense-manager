64b7c249c901f6a02d2dbc99b31b1705f7d2e59e
MohdFaique <mdfaique5@gmail.com>
2023-07-15 19:51:40 +0530

    header mdf

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,45 @@
-const Header = ( props ) => {
+import React, { useState } from "react";
+import Button from "../button";
+import userData from "./userData";
+import "./style.css";
 
-}
+const Header = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const filteredUser  = userData.filter(user => user.name === 'JohnDoe')[0];
+  
+
+
+  const handleDropdownToggle = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleLogout = () => {
+    // Handle logout functionality here
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="/favicon.ico" alt="Logo" />
+      </div>
+      <div className="header-right">
+        <Button className="dashboard-button" title="Dashboard" />
+        <div className="user-dropdown">
+          <Button
+            className="user-button"
+            onClick={handleDropdownToggle}
+            title={filteredUser.name}
+          />
+          {showDropdown && (
+            <div className="dropdown-content">
+              <Button onClick={handleLogout} title="Logout"/>
+              {/* Add more dropdown items here */}
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
---
+++ src/components/header/userData.js
@@ -0,0 +1,8 @@
+const userData = [
+    { name: 'JohnDoe', email: 'johndoe@example.com', phone: '123' },
+    { name: 'JaneSmith', email: 'janesmith@example.com', phone: '987-654-3210' },
+    // Add more user objects as needed
+  ];
+  
+  export default userData;
+  
