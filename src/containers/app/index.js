2b401a1b53cdac57238f211c82032a4bbb8252fb
harshitab07 <07harshu07@gmail.com>
2023-07-16 20:50:57 +0530

    body style added

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { allRoutes } from "./all-routes";
+import './style.css';
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 
