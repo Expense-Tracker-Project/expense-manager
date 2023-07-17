3c7b8e6e8e9bf4d7c718e51145c17884f245248e
harshitab07 <07harshu07@gmail.com>
2023-07-17 09:48:29 +0530

    color palette changed

--- src/containers/signup/index.js
+++ src/containers/signup/index.js
@@ -1,5 +1,9 @@
+import SignupData from "../../data/signup";
+import Form from "../../components/form";
 const Signup = ( props ) => {
-
+return (
+    <Form items={SignupData} isSignup={true} button={'Sign up'} passwordLabel={'Re-enter Password'} />
+)
 }
 
 export default Signup;
