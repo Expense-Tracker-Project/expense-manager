c187addbc934876df6abd9fe73a7ce7570f224ad
harshitab07 <harshitabhatt1730@gmail.com>
2023-08-15 22:58:29 +0530

    extra things removed

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -4,11 +4,6 @@ export const checkEmail = (email) => String(email)
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   );
 
-const invalidDomains = ['@eurokool.com'];
-
-export const verifyFormDetails = (email) => !invalidDomains.filter((ele) => email.includes(ele)).length;
-
-
 export const numbersOnly = (string) => {
     const pattern = /^[0-9]+$/;
     return pattern.test(string);
