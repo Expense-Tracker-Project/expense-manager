cf20fc328b0fb0dfdf9b4a11cd3efcbd9df41906
harshitab07 <harshitabhatt1730@gmail.com>
2023-08-17 22:55:30 +0530

    login signup backend setup

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,31 +1,61 @@
 import "../signup/style.css";
 import Password from "../../components/password";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/button";
+import axios from "axios";
 
 const Login = (props) => {
-    const [name, setName] = useState('');
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleSubmit = async(e) => {
+      e.preventDefault();
+
+      try {
+        await axios.post("http://localhost:8000/login", {
+          email, password
+        })
+        .then(res => {
+          if (res.data === "incorrect password") {
+            alert("Incorrect password");
+          } else if (res.data !== "not exist") {
+            navigate("/", {state: {id: res.data.name}});
+          } else if (res.data === "not exist") {
+            alert("user has not signed up");
+          }
+        })
+        .catch(e => {
+          alert("Wrong details");
+          console.log(e);
+        })
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
   return (
     <div className="signup-wrapper">
+      <form>
       <div className="form-component">
         <div className="form-elements">
           <div className="form-element">
-            <label htmlFor="name" key="name">
-              <span>Name</span>
+            <label htmlFor="email" key="email">
+              <span>Email</span>
               <div className="form-input-container">
-                <input className="custom-form-input" type="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                <input className="custom-form-input" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
               </div>
             </label>
           </div>
           <div className="form-element">
           <Password isSignup={false} passwordLabel="Re-Enter Password" setPassword={setPassword} />
           </div>
-          <Button title="Sign in" />
+          <input type="submit" className="custom-button" onClick={handleSubmit} />
         </div>
         <div className="bottom-text">Doesn't have account? <Link to='/signup' className="signup-link">Signup</Link></div>
       </div>
+      </form>
       <img src="./piggi.png" />
     </div>
   );
--- src/containers/signup/index.js
+++ src/containers/signup/index.js
@@ -2,15 +2,43 @@ import "./style.css";
 import Password from "../../components/password";
 import { useState } from "react";
 import Button from "../../components/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signup = (props) => {
+  const navigate = useNavigate();
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleSubmit = async(e) => {
+      e.preventDefault();
+
+      try {
+        await axios.post("http://localhost:8000/signup", {
+          email, password, name
+        })
+        .then(res => {
+          if (res.data === "exist") {
+            alert("user already exists");
+          } else if (res.data === "not exist") {
+            navigate("/", {state: {id: name}});
+          }
+        })
+        .catch(e => {
+          alert("Wrong details");
+          console.log(e);
+        })
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+
   return (
     <div className="signup-wrapper">
       <div className="form-component">
+        <form>
         <div className="form-elements">
           <div className="form-element">
             <label htmlFor="name" key="name">
@@ -31,8 +59,9 @@ const Signup = (props) => {
           <div className="form-element">
           <Password isSignup={true} passwordLabel="Re-Enter Password" setPassword={setPassword} />
           </div>
-          <Button title="Sign Up" />
+          <input type="submit" className="custom-button" onClick={handleSubmit} />
         </div>
+        </form>
         <div className="bottom-text">Already have account? <Link to='/login' className="signup-link">Sign in</Link></div>
       </div>
       <img src="./piggi.png" />
